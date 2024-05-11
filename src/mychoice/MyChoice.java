c567e881244cc167550cd7b164e7dad127e385f5
NGUYENTHIHONGTUYET09 <nguyenthihongtuyet004@gmail.com>
2024-05-11 23:57:32 +0700

    JAVA GUI frame

---
+++ src/mychoice/MyChoice.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mychoice;
+
+import java.awt.Frame;
+
+/**
+ *
+ * @author minhtan
+ */
+public class MyChoice extends Frame implements ActionListener, ItemListener{
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
